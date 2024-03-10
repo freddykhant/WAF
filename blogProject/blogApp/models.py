89c4baef040093cc0083a0f7a1aaa8af2df4edcf
Freddy Khant <105434008+freddykhant@users.noreply.github.com>
2024-03-10 21:17:49 +0800

    more answers, model class and methods created, migrations made and applied

--- blogProject/blogApp/models.py
+++ blogProject/blogApp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime
 
 # Create your models here.
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    publication_date = models.DateTimeField(auto_now_add=True)
+    tag = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=255, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    def is_pre_covid(self):
+        # Defining the cutoff date for pre-COVID-19
+        covid_start_date = datetime.date(2020, 3, 10)
+        return self.publication_date.date() <= covid_start_date
+
+    def __str__(self):
+        return self.title
