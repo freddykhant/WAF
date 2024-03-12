848284712e104250c6a35ea57d54957c65bd6366
Freddy Khant <105434008+freddykhant@users.noreply.github.com>
2024-03-12 22:38:30 +0800

    debugged html files

--- assignment1/curtin/courses/urls.py
+++ assignment1/curtin/courses/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     #path('', views.index, name='index')
-    path('courses/', views.course_list, name='course_list'),
     path('schools/', views.school_list, name='school_list'),
-    path('schools/<int:school_id>/', views.school_detail, name='school_detail'),
+    path('courses/', views.course_list, name='course_list'),
 ]
