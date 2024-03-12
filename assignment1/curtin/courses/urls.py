0601ebb47be65bc341b53d2b4abe0ad769a07bea
Freddy Khant <105434008+freddykhant@users.noreply.github.com>
2024-03-12 22:19:00 +0800

    finished creating the views methods for retrieving list of schools/courses and mapped them successfully

--- assignment1/curtin/courses/urls.py
+++ assignment1/curtin/courses/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
-    #path('courses/', views.courses_list, name='courses_list'),
-    #path('schools/', views.schools_list, name='schools_list'),
-    #path('schools/<int:school_id>/', views.school_detail, name='school_detail'),
+    #path('', views.index, name='index')
+    path('courses/', views.courses_list, name='courses_list'),
+    path('schools/', views.schools_list, name='schools_list'),
+    path('schools/<int:school_id>/', views.school_detail, name='school_detail'),
 ]
--- assignment1/curtin/courses/views.py
+++ assignment1/curtin/courses/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_list_or_404
+#from django.http import HttpResponse
+from .models import Course, School
+
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, World!")
+#def index(request):
+#    return HttpResponse("Hello, World!")
+
+def courses_list(request):
+    courses = Course.objects.all()
+    return render(request, 'courses/courses_list.html', {'course' : courses})
+
+def schools_list(request):
+    schools = School.objects.all()
+    return render(request, 'courses/schools_list.html', {'school' : schools})
+
+def school_detail(request, school_id):
+    school = get_list_or_404(School, id=school_id)
+    return render(request, 'courses/school_detail.html', {'school' : school})
+
