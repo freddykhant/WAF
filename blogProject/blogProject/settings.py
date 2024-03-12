04e96fe1b6979dc2bb52ee6bd99376867ba7edd8
Freddy Khant <105434008+freddykhant@users.noreply.github.com>
2024-03-12 18:49:21 +0800

    changed debug to false

--- blogProject/blogProject/settings.py
+++ blogProject/blogProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&+9y(gf0lr^_ar*3bv(c((e)fqbcm06oj^t=@e^!0q&k=g0gkj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
