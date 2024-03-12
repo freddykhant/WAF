891e66eebcb7ac6e8a0e09f9042060af1e896fd1
Freddy Khant <105434008+freddykhant@users.noreply.github.com>
2024-03-13 00:01:15 +0800

    admin functions added

--- assignment1/curtin/courses/admin.py
+++ assignment1/curtin/courses/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import School, Course
 
 # Register your models here.
-#admin.site.register(School)
-#admin.site.register(Course)
+admin.site.register(School)
+admin.site.register(Course)
