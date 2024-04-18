6b18af9e6c67126d69b0dd3329ac7b641e97febe
Freddy Khant <105434008+freddykhant@users.noreply.github.com>
2024-04-18 13:09:02 +0800

    serving the view, making http post requests in the view, handling them server side

--- EasyToDo/app.js
+++ EasyToDo/app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res) {
-    res.send("Hello World!!", );
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, function(){
-    console.log('Listening on port 3000')
+app.listen(3000, function() {
+    console.log('Listening on port 3000');
+});
+
+app.post('/todos', function(req, res){
+    console.log('POST request received');
 });
